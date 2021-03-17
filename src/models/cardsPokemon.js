e3d8b971d887f9394e6ffa26ecbe0f18276db067
Randy <phpgeweld@gmail.com>
2021-03-17 13:09:54 +0100

    new model methods

--- src/models/cardsPokemon.js
+++ src/models/cardsPokemon.js
@@ -4,6 +4,26 @@ module.exports = {
 
     table: 'cards_pokemon',
 
+    async get(ID)
+    {
+        return new Promise(resolve => {
+           db.query(`SELECT * FROM ${this.table} WHERE id = ?`, [ID], (err, rows) => {
+               if(err) console.log(err);
+               else resolve(rows[0]);
+           });
+        });
+    },
+
+    async getForName(name)
+    {
+        return new Promise(resolve => {
+            db.query(`SELECT * FROM ${this.table} WHERE \`name\` = ?`, [name], (err, rows) => {
+                if(err) console.log(err);
+                else resolve(rows);
+            });
+        });
+    },
+
     async getRandom()
     {
         return new Promise(resolve => {
