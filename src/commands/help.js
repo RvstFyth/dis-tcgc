0665776b3fd14abb37f3b19aea3b282456c354c3
Randy <phpgeweld@gmail.com>
2021-03-18 21:55:51 +0100

    updated help files

--- src/commands/help.js
+++ src/commands/help.js
@@ -2,6 +2,22 @@ module.exports = {
 
     async run(msg, args, data)
     {
-        return msg.channel.send(`**TODO** help file`);
+        let description = `` +
+            `\`,start\` Creates a account and start collecting cards!\n` +
+            `\`,cards\` See the cards you collected\n` +
+            `\`,card [id]\` Query card data\n` +
+            `\`,cards -d\` Shows your duplicate cards\n` +
+            `\`,sets\` Shows all sets that are in the game\n` +
+            `\`,set [name]\` Shows all cards in a set and how many you own\n` +
+            `\`,set [name] -m\` Shows all cards you are missing from a set\n` +
+            `\`,set [name] -o\` Shows all cards you own from a set`;
+
+
+        const embed = {
+            title: 'help',
+            description
+        }
+
+        return msg.channel.send({embed});
     }
 }
