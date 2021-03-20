3a7470044f3c03981577d5ed65df29f3330b39d7
Randy <phpgeweld@gmail.com>
2021-03-20 11:42:45 +0100

    display owned cards in card command

--- src/commands/card.js
+++ src/commands/card.js
@@ -1,32 +1,43 @@
 const cardsPokemonModel = require('../models/cardsPokemon');
+const usersCardsPokemonModel = require('../models/usersCardsPokemon');
 
 module.exports = {
-
     async run(msg, args, data) {
-        if(isNaN(args[0])) return msg.channel.send(`**${msg.author.username}** this command only works with IDs. Use \`${data.prefix}search [name]\` to find IDs.`);
+        if (isNaN(args[0]))
+            return msg.channel.send(
+                `**${msg.author.username}** this command only works with IDs. Use \`${data.prefix}search [name]\` to find IDs.`
+            );
 
         const ID = parseInt(args[0]);
         const card = await cardsPokemonModel.get(ID);
-        if(!card) return msg.channel.send(`**${msg.author.username}** there is no card with ID ${ID}.`);
+        if (!card)
+            return msg.channel.send(
+                `**${msg.author.username}** there is no card with ID ${ID}.`
+            );
         const imagePathSplitted = card.image_large.split('/');
 
-        let description = `` +
-            `Set: ${card.set}\n` +
-            `ID: ${card.id}`;
-
+        let description = `` + `Set: ${card.set}\n` + `ID: ${card.id}`;
+        const userCards = await usersCardsPokemonModel.getForUser(msg.author.id, card.id);
         const embed = {
             title: card.name,
             description,
             image: {
-                url: `attachment://${imagePathSplitted[imagePathSplitted.length - 1]}`
+                url: `attachment://${
+                    imagePathSplitted[imagePathSplitted.length - 1]
+                }`,
+            },
+            footer: {
+                text: `You own ${userCards ? userCards.amount : 0}`
             }
         };
-        await msg.channel.send( {
+        await msg.channel.send({
             embed,
-            files: [{
-                attachment: card.image_large,
-                name: imagePathSplitted[imagePathSplitted.length - 1]
-            }]
+            files: [
+                {
+                    attachment: card.image_large,
+                    name: imagePathSplitted[imagePathSplitted.length - 1],
+                },
+            ],
         });
-    }
+    },
 };
