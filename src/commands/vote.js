02b574b41f511014759a35866f52c4ddd9d5793e
RvStFyth <phpgeweld@gmail.com>
2021-04-02 19:07:32 +0200

    Changed vote command output

--- src/commands/vote.js
+++ src/commands/vote.js
@@ -5,7 +5,7 @@ module.exports = {
         const embed = {
             title: 'Vote',
             description: `[You can vote for this bot on top.gg](https://top.gg/bot/821802913533657138/vote).\n` +
-                `This will reset your daily cooldown and you get a random booster for it!`
+                `This will reset your daily, so you can claim it again!`
         };
 
         return msg.channel.send({embed});
