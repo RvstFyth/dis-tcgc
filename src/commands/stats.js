839fc5b695baf2b19b5f4cc6feff56f84b70dd81
RvStFyth <phpgeweld@gmail.com>
2021-04-05 17:24:54 +0200

    updated stats command

--- src/commands/stats.js
+++ src/commands/stats.js
@@ -7,10 +7,13 @@ module.exports = {
         const common = await userCardsPokemonModel.getTotalForUserAndRarity(msg.author.id, 'common');
         const uncommon = await userCardsPokemonModel.getTotalForUserAndRarity(msg.author.id, 'uncommon');
         const rare = await userCardsPokemonModel.getTotalForUserAndRarity(msg.author.id, 'rare');
+        const promo = await userCardsPokemonModel.getTotalForUserAndRarity(msg.author.id, 'promo');
+        const legends = await userCardsPokemonModel.getTotalForUserAndRarity(msg.author.id, 'legends');
+
         const total = await userCardsPokemonModel.getTotalCollectedForUser(msg.author.id);
 
         const fields = [];
-        const uniqueField = { name: 'Cards', value: `Common: ${common}\nUncommon: ${uncommon}\nRare: ${rare}\nTotal unique: ${common + uncommon + rare}\nTotal: ${total}`, inline: true };
+        const uniqueField = { name: 'Cards', value: `Common: ${common}\nUncommon: ${uncommon}\nRare: ${rare}\nPromo: ${promo}\nLegends: ${legends}\nTotal unique: ${common + uncommon + rare + promo + legends}\nTotal: ${total}`, inline: true };
         // const extraField = { name: '\u200b', value: }
 
         fields.push(uniqueField);
