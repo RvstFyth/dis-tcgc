9b3f60fb9e5a1fc91fd078bf37b8f53c096c3f50
RvStFyth <phpgeweld@gmail.com>
2021-04-17 19:25:35 +0200

    fixed set command with sets with hte word 'set' in it

--- src/commands/set.js
+++ src/commands/set.js
@@ -2,37 +2,62 @@ const cardsPokemonModel = require('../models/cardsPokemon');
 const setsPokemonModel = require('../models/setsPokemon');
 
 const emojis = {
-    next: '▶️'
+    next: '▶️',
 };
 
 module.exports = {
+    async run(msg, args, data) {
+        args = msg.content.toLowerCase().split(' ').splice(1);
 
-    async run(msg, args, data)
-    {
-        const owned = args.filter(a => ['-owned', '-o'].indexOf(a) > -1).length;
-        const missing = args.filter(a => ['-missing', '-m'].indexOf(a) > -1).length;
-        const duplicates = args.filter(a => ['-duplicates', '-d'].indexOf(a) > -1).length;
+        const owned = args.filter((a) => ['-owned', '-o'].indexOf(a) > -1)
+            .length;
+        const missing = args.filter((a) => ['-missing', '-m'].indexOf(a) > -1)
+            .length;
+        const duplicates = args.filter(
+            (a) => ['-duplicates', '-d'].indexOf(a) > -1
+        ).length;
 
-        args = args.filter(a => !a.startsWith('-'));
+        args = args.filter((a) => !a.startsWith('-'));
 
-        if(!args[0]) return msg.channel.send(`**${msg.author.username}** not enough arguments..`);
+        if (!args[0])
+            return msg.channel.send(
+                `**${msg.author.username}** not enough arguments..`
+            );
 
         const input = args.join(' ');
         let cards;
-        if(owned) cards = await cardsPokemonModel.getCardsForSetForUserOwned(input, msg.author.id);
-        else if(missing) cards = await cardsPokemonModel.getCardsForSetForUserMissing(input, msg.author.id);
-        else if(duplicates) cards = await cardsPokemonModel.getCardsForSetForUserDuplicates(input, msg.author.id);
-        else cards = await cardsPokemonModel.getCardsForSetForUser(input, msg.author.id);
+        if (owned)
+            cards = await cardsPokemonModel.getCardsForSetForUserOwned(
+                input,
+                msg.author.id
+            );
+        else if (missing)
+            cards = await cardsPokemonModel.getCardsForSetForUserMissing(
+                input,
+                msg.author.id
+            );
+        else if (duplicates)
+            cards = await cardsPokemonModel.getCardsForSetForUserDuplicates(
+                input,
+                msg.author.id
+            );
+        else
+            cards = await cardsPokemonModel.getCardsForSetForUser(
+                input,
+                msg.author.id
+            );
 
-        if(!cards || !cards.length) return msg.channel.send(`**${msg.author.username}** no cards found..`);
+        if (!cards || !cards.length)
+            return msg.channel.send(
+                `**${msg.author.username}** no cards found..`
+            );
 
         const pSet = await setsPokemonModel.getForName(input);
 
         return this.postEmbed(msg, cards, pSet);
     },
 
-    async postEmbed(msg, data, pSet, page = 1, originalEmbed = null)
-    {
+    async postEmbed(msg, data, pSet, page = 1, originalEmbed = null) {
         const limit = 10;
         const maxPage = Math.ceil(data.length / limit);
         const offset = page * limit - limit;
@@ -41,53 +66,74 @@ module.exports = {
         const cards = dData.splice(offset, limit);
         let description = '';
 
-        for(let i in cards) {
-            description += `#${cards[i].id} - ${cards[i].name} (${cards[i].amount || 0})\n`;
+        for (let i in cards) {
+            description += `#${cards[i].id} - ${cards[i].name} (${
+                cards[i].amount || 0
+            })\n`;
         }
 
-        if(!description) description = '...';
+        if (!description) description = '...';
 
         // Test
         const embed = {
             title: `${cards[0].set}`,
             description,
             thumbnail: {
-                url: pSet.symbol
+                url: pSet.symbol,
             },
             image: {
-                url: pSet.logo
+                url: pSet.logo,
             },
             footer: {
-                text: `Page: ${page}/${maxPage} | ${data.length} total in this set`
-            }
+                text: `Page: ${page}/${maxPage} | ${data.length} total in this set`,
+            },
         };
-        if(originalEmbed) {
-            return originalEmbed.edit({embed}).then(async message => {
+        if (originalEmbed) {
+            return originalEmbed.edit({ embed }).then(async (message) => {
                 if (page < maxPage) {
                     await message.react(emojis.next);
-                    const filter = (reaction, user) => reaction.emoji.name === emojis.next && user.id === msg.author.id;
-                    message.awaitReactions(filter, {time: 60000, max: 1}).then(collected => {
-                        const reaction = collected.first();
-                        if (reaction) {
-                            return this.postEmbed(msg, data, pSet, page + 1, message);
-                        }
-                    });
+                    const filter = (reaction, user) =>
+                        reaction.emoji.name === emojis.next &&
+                        user.id === msg.author.id;
+                    message
+                        .awaitReactions(filter, { time: 60000, max: 1 })
+                        .then((collected) => {
+                            const reaction = collected.first();
+                            if (reaction) {
+                                return this.postEmbed(
+                                    msg,
+                                    data,
+                                    pSet,
+                                    page + 1,
+                                    message
+                                );
+                            }
+                        });
+                }
+            });
+        } else {
+            return msg.channel.send({ embed }).then(async (message) => {
+                if (page < maxPage) {
+                    await message.react(emojis.next);
+                    const filter = (reaction, user) =>
+                        reaction.emoji.name === emojis.next &&
+                        user.id === msg.author.id;
+                    message
+                        .awaitReactions(filter, { time: 60000, max: 1 })
+                        .then((collected) => {
+                            const reaction = collected.first();
+                            if (reaction) {
+                                return this.postEmbed(
+                                    msg,
+                                    data,
+                                    pSet,
+                                    page + 1,
+                                    message
+                                );
+                            }
+                        });
                 }
             });
         }
-        else {
-            return msg.channel.send({embed}).then(async message => {
-                if (page < maxPage) {
-                    await message.react(emojis.next);
-                    const filter = (reaction, user) => reaction.emoji.name === emojis.next && user.id === msg.author.id;
-                    message.awaitReactions(filter, {time: 60000, max: 1}).then(collected => {
-                        const reaction = collected.first();
-                        if (reaction) {
-                            return this.postEmbed(msg, data, pSet, page + 1, message);
-                        }
-                    });
-                }
-            });
-        }
-    }
+    },
 };
