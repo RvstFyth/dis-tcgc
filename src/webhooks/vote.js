78864f35a284714beb6493772fb49d942bbd01ef
RvStFyth <phpgeweld@gmail.com>
2021-04-20 20:31:40 +0200

    Added reminder option to vote webhook

--- src/webhooks/vote.js
+++ src/webhooks/vote.js
@@ -1,5 +1,8 @@
 const usersModel = require('../models/users');
 const cooldownsModel = require('../models/cooldowns');
+const valuesHelper = require('../helpers/values');
+const remindersModel = require('../models/reminders');
+const reminderEmoji = '⏰';
 
 module.exports = {
     async run(bot, userID, weekend) {
@@ -8,9 +11,31 @@ module.exports = {
             await cooldownsModel.setDaily(userID, true);
             const discUser = await bot.users.fetch(userID);
             if (discUser) {
-                await discUser.send(
-                    `Thank you for voting for your support! You daily is \`,daily\` is reset, go claim it again!`
-                );
+                await discUser
+                    .send(
+                        `Thank you for voting for your support! You daily is \`,daily\` is reset, go claim it again!`
+                    )
+                    .then(async (message) => {
+                        await message.react(reminderEmoji);
+                        const filter = (reaction, user) =>
+                            reaction.emoji.name === reminderEmoji &&
+                            user.id === discUser.id;
+                        message
+                            .awaitReactions(filter, { time: 60000, max: 1 })
+                            .then(async (collected) => {
+                                const reaction = collected.first();
+                                if (reaction) {
+                                    await remindersModel.create(
+                                        discUser.id,
+                                        'You can vote again!',
+                                        valuesHelper.currentTimestamp() +
+                                            3600 * 12
+                                    );
+                                }
+                            })
+                            .catch((e) => console.log(e));
+                    })
+                    .catch((e) => console.log(e));
             }
         }
     },
