04de9f24a0f4d117bffdf0bc0627874c18156c9d
RvStFyth <phpgeweld@gmail.com>
2021-05-23 18:53:10 +0200

    reversed shop changes

--- src/commands/shop.js
+++ src/commands/shop.js
@@ -5,8 +5,6 @@ const shopModel = require('../models/shop');
 const valuesHelper = require('../helpers/values');
 const setsModel = require('../models/setsPokemon');
 
-const boosterPrice = 250;
-
 module.exports = {
     aliasses: ['buy'],
     async run(msg, args, data) {
@@ -20,7 +18,7 @@ module.exports = {
             );
 
         const offers = await shopModel.getActive();
-        const activeField = { name: '\u200b', value: '' };
+        const activeField = { name: 'Sales (-25%)', value: '' };
         let cnt = 1;
         for (let i in offers) {
             activeField.value += `**${cnt}**: ${
@@ -38,7 +36,7 @@ module.exports = {
             description:
                 `Expand your collection by buying booster packs! Each time a card drops` +
                 ` you get a random amount of coins. You can use these coins to buy booster packs. ` +
-                `Each booster gives 10 new cards, and costs 250 coins. Each day a booster is replaced in the shop`,
+                `Each booster gives 10 new cards, and costs 250 coins.`,
             fields: [
                 activeField,
                 {
@@ -61,32 +59,30 @@ module.exports = {
     },
 
     async buy(msg, args, data) {
-        let amount = 1;
+        let amount = 1,
+            boosterPrice = 250;
+
         if (args[0] && !isNaN(args[0]) && args[0] > 0) {
             amount = parseInt(args[0]);
             args = args.splice(1);
         }
 
-        if (data.user.coins < boosterPrice * amount)
-            return msg.channel.send(
-                `**${msg.author.username}** you don't have enough coins (${
-                    data.user.coins
-                }), you need ${
-                    boosterPrice * amount
-                } coins to buy ${amount} x booster. You get coins when a card drops or with bonus commands.`
-            );
         const input = args.join(' ');
         const set = await setsModel.getForName(input);
         if (!set)
             return msg.channel.send(
                 `**${msg.author.username}** invalid set name provided..`
             );
-        const activeOffer = await shopModel.getActiveForSetID(set.id);
-        if (!activeOffer) {
+
+        const activeSale = await shopModel.getActiveForSetID(set.id);
+        if (activeSale) boosterPrice *= 0.75;
+        const price = boosterPrice * amount;
+
+        if (data.user.coins < price)
             return msg.channel.send(
-                `**${msg.author.username}** these boosters are not for sale now. See \`,shop\` to see which boosters are for sale.`
+                `**${msg.author.username}** you don't have enough coins (${data.user.coins}), you need ${price} coins to buy ${amount} x booster. You get coins when a card drops or with bonus commands.`
             );
-        }
+
         await usersModel.setCoins(
             msg.author.id,
             parseInt(data.user.coins) - boosterPrice * amount
