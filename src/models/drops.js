938f4b1c5f3bfbd96082a2a46f2ec82dead99650
RvStFyth <phpgeweld@gmail.com>
2021-06-06 21:51:44 +0200

    prevent cards dropping within set timeframe

--- src/models/drops.js
+++ src/models/drops.js
@@ -1,26 +1,38 @@
-const db = require("../db").getConnection();
-const valuesHelper = require("../helpers/values");
+const db = require('../db').getConnection();
+const valuesHelper = require('../helpers/values');
 
 module.exports = {
-  table: "drops",
-  async create(userID, guildID, channelID, card, coins) {
-    return new Promise((resolve) => {
-      const values = [
-        userID,
-        guildID,
-        channelID,
-        card,
-        coins,
-        valuesHelper.currentTimestamp(),
-      ];
-      db.query(
-        `INSERT INTO ${this.table} (user_id, guild_id, channel_id, card, coins, \`timestamp\`) VALUES (?,?,?,?,?,?)`,
-        values,
-        (err) => {
-          if (err) console.log(err);
-          else resolve(true);
-        }
-      );
-    });
-  },
+    table: 'drops',
+    async create(userID, guildID, channelID, card, coins) {
+        return new Promise((resolve) => {
+            const values = [
+                userID,
+                guildID,
+                channelID,
+                card,
+                coins,
+                valuesHelper.currentTimestamp(),
+            ];
+            db.query(
+                `INSERT INTO ${this.table} (user_id, guild_id, channel_id, card, coins, \`timestamp\`) VALUES (?,?,?,?,?,?)`,
+                values,
+                (err) => {
+                    if (err) console.log(err);
+                    else resolve(true);
+                }
+            );
+        });
+    },
+    async getLastForUserID(userID) {
+        return new Promise((resolve) => {
+            db.query(
+                `SELECT * FROM ${this.table} WHERE user_id = ? ORDER BY id DESC LIMIT 1`,
+                [userID],
+                (err, rows) => {
+                    if (err) console.log(err);
+                    else resolve(rows[0]);
+                }
+            );
+        });
+    },
 };
