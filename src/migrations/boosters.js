4a3cbdcc16cbfd2840bf01b107bb74f19debc35d
RvStFyth <phpgeweld@gmail.com>
2021-06-20 19:43:36 +0200

    migration fille for migrating to new boosters setup

---
+++ src/migrations/boosters.js
@@ -0,0 +1,34 @@
+const util = require('util');
+const db = require('../db');
+db.init();
+
+const con = db.getConnection();
+
+// node native promisify
+const query = util.promisify(con.query).bind(con);
+
+(async () => {
+    try {
+        const users = await query(`SELECT * FROM users`);
+        for (let i in users) {
+            const boosters = await query(
+                `SELECT booster, COUNT(*) AS total FROM users_boosters_pokemon WHERE user_id = ${users[i].discord_id} GROUP BY booster`
+            );
+            for (let j in boosters) {
+                const set = await query(
+                    `SELECT * FROM sets_pokemon WHERE LOWER(name) = '${boosters[
+                        j
+                    ].booster.toLowerCase()}'`
+                );
+                if (set[0])
+                    await query(
+                        `INSERT INTO boosters2 (user_id, set_id, amount) VALUES (?,?,?)`,
+                        [users[i].discord_id, set[0].id, boosters[j].total]
+                    );
+            }
+        }
+    } finally {
+        // console.log('Done')
+        con.end();
+    }
+})();
