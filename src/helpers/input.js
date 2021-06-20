d68bf4694fd462f74e4712bbfe31c9ad86b236ce
RvStFyth <phpgeweld@gmail.com>
2021-06-20 14:48:36 +0200

    Added a cancel reaction to input helper

--- src/helpers/input.js
+++ src/helpers/input.js
@@ -1,4 +1,5 @@
 const confirmEmoji = '✅';
+const cancelEmoji = '🔴';
 
 module.exports = {
     async askUserToConfirm(description, msg, removeAfterTimeout = false) {
@@ -12,14 +13,20 @@ module.exports = {
                 .send({ embed })
                 .then(async (message) => {
                     await message.react(confirmEmoji);
+                    await message.react(cancelEmoji);
                     const filter = (reaction, user) =>
-                        reaction.emoji.name === confirmEmoji &&
-                        user.id === msg.author.id;
+                        [confirmEmoji, cancelEmoji].indexOf(
+                            reaction.emoji.name
+                        ) > -1 && user.id === msg.author.id;
                     message
                         .awaitReactions(filter, { max: 1, time: 30 * 1000 })
                         .then(async (collected) => {
                             const reaction = collected.first();
-                            if (reaction) resolve(true);
+                            if (
+                                reaction &&
+                                reaction.emoji.name === confirmEmoji
+                            )
+                                resolve(true);
                             else {
                                 if (removeAfterTimeout) await message.delete();
                                 resolve(false);
